c7473bc443927fb02fb8674ef3b0603ea11d0f04
mud <mudmud.muhd@gmail.com>
2022-08-09 04:34:41 +0800

    in progress - debugging movement restrictions

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = {
     level: 0,
     original: true,
     allCoordinates: [],
+    // prevCoordinates: [],
     currentMap: [],
     maps: [//put in separate js file and import/export eg export function setupCounter(element) and  imprt {setupCounter} from 'file.js'
         `####
@@ -64,6 +65,7 @@ const paintMap = () => {
         app.currentMap = originalMapProcessing()
     }
     const $body = $('body')
+    $body.empty()
     const $map = $('<section>').addClass('map')
     for (const row of app.currentMap) { //for each row of map
         const $section = $('<section>').addClass('row')//each section represents a row
@@ -111,20 +113,25 @@ const playerOriginalPosition = () => {
 
 
 
-paintMap()
+paintMap()//can be used for reset
 //switch case
 window.onkeydown = (event) => { //finds out which arrow key is pressed and updates playerPosition, precedingPoint, and followingPoint
-    paintMap()
+    // paintMap()
     let playerPosition = playerOriginalPosition()
-    let precedingPoint = [] //for behind new player position
-    let followingPoint = [] //for infront of new player position
-    let allCoordinates = [] //compile all coordinates
-    if (!app.original) {
-        precedingPoint = app.allCoordinates[0]
-        playerPosition = app.allCoordinates[1]
-        followingPoint = app.allCoordinates[2]
-        console.log('here')
-    }
+    let coordinates = [] //compile all coordinates
+    let precedingPoint = []
+    let followingPoint = []
+
+    // if (!app.original) {
+    //     precedingPoint = app.allCoordinates[0]
+    //     playerPosition = app.allCoordinates[1]
+    //     followingPoint = app.allCoordinates[2]
+    //     coordinates.push(precedingPoint, playerPosition, followingPoint)
+
+    //     console.log('here')
+    // }
+    // app.prevCoordinates = [precedingPoint, playerPosition, followingPoint]
+
     if (event.key === 'ArrowUp') {
         precedingPoint[0] = playerPosition[0]
         precedingPoint[1] = playerPosition[1]
@@ -134,8 +141,6 @@ window.onkeydown = (event) => { //finds out which arrow key is pressed and updat
         followingPoint[0] = playerPosition[0] - 1
         followingPoint[1] = playerPosition[1]
 
-        allCoordinates.push(precedingPoint, playerPosition, followingPoint)
-        app.allCoordinates = allCoordinates
         console.log('up')
     } else if (event.key === 'ArrowDown') {
         precedingPoint[0] = playerPosition[0]
@@ -146,8 +151,6 @@ window.onkeydown = (event) => { //finds out which arrow key is pressed and updat
         followingPoint[0] = playerPosition[0] + 1
         followingPoint[1] = playerPosition[1]
 
-        allCoordinates.push(precedingPoint, playerPosition, followingPoint)
-        app.allCoordinates = allCoordinates
         console.log('down')
     } else if (event.key === 'ArrowLeft') {
         precedingPoint[0] = playerPosition[0]
@@ -158,8 +161,6 @@ window.onkeydown = (event) => { //finds out which arrow key is pressed and updat
         followingPoint[0] = playerPosition[0]
         followingPoint[1] = playerPosition[1] - 1
 
-        allCoordinates.push(precedingPoint, playerPosition, followingPoint)
-        app.allCoordinates = allCoordinates
         console.log('left')
     } else if (event.key === 'ArrowRight') {
         precedingPoint[0] = playerPosition[0]
@@ -170,14 +171,45 @@ window.onkeydown = (event) => { //finds out which arrow key is pressed and updat
         followingPoint[0] = playerPosition[0]
         followingPoint[1] = playerPosition[1] + 1
 
-        allCoordinates.push(precedingPoint, playerPosition, followingPoint)
-        app.allCoordinates = allCoordinates
         console.log('right')
     }
+
+    coordinates[0] = precedingPoint
+    coordinates[1] = playerPosition
+    coordinates[2] = followingPoint
+
+    const restrictedRow = coordinates[1][0]
+    const restrictedColumn = coordinates[1][1]
+    const restrictedRow1 = coordinates[2][0]
+    const restrictedColumn1 = coordinates[2][1]
+    let restrictions = ''
+    let restrictions1 = ''
+    if (restrictedRow >= 0 && restrictedColumn >= 0 && restrictedRow1 >= 0 && restrictedColumn1 >= 0 ) {
+        restrictions = app.currentMap[restrictedRow][restrictedColumn] //check for restrictions of player movement
+        restrictions1 = app.currentMap[restrictedRow1][restrictedColumn1] //check for restrictions based on following point
+        // console.log(restrictions)
+        // console.log(restrictions1)
+        if (restrictions === '#' || ((restrictions === '$' || restrictions === '*') && (restrictions1 === '#' || restrictions1 === '$' || restrictions1 === '*'))) {//check restriction: whether the player has a wall infront of them or not
+            console.log('in grid but move restricted')
+            // console.log(app.allCoordinates)
+        } else {
+            app.allCoordinates = coordinates
+            console.log('dapat move')
+            // console.log(app.allCoordinates)
+            updateSymbols()
+            paintMap()
+        }
+    } else {
+        console.log('over the grid alr')
+        app.allCoordinates = []
+        // console.log(app.allCoordinates)
+    }
+
+
     app.original = false//!!!!!!!!!!!!!!!!!!!!!!!!!!double check
-    console.log(app.allCoordinates)
-    updateSymbols()
+    // console.log(app.allCoordinates)
     // render()
+    
 
 }
 
@@ -190,36 +222,37 @@ window.onkeydown = (event) => { //finds out which arrow key is pressed and updat
 //change to switch case
 //check restriction and change symbols
 const updateSymbols = () => {
-    const playerPosition = app.allCoordinates[0]
-    if (playerPosition === '#' || ((playerPosition === '$' || playerPosition === '*') && (followingPoint === '#' || followingPoint === '$' || followingPoint === '*'))) {//check restriction: whether the player has a wall infront of them or not
-        console.log('move restricted')
-    } else {
-        for (let i = 0; i < app.allCoordinates.length; i++) {//change....
-            const row = app.allCoordinates[i][0]
-            const column = app.allCoordinates[i][1]
-            const point = app.currentMap[row][column]
+    for (let i = 2; i>=0; i--) {//change....
+        const row = app.allCoordinates[i][0]
+        const column = app.allCoordinates[i][1]
+        const prevRow = app.allCoordinates[1][0]
+        const prevColumn = app.allCoordinates[1][1]
+        const prevPoint = app.currentMap[prevRow][prevColumn]
+        if (row >= 0 && column >= 0) {
+            let point = app.currentMap[row][column]
+            console.log(point)
             if (i === 0) {//preceding points can only change to floor or goal
                 if (point === '@') { //player
-                    point = ' ' //change to floor
+                    app.currentMap[row][column] = ' ' //change to floor
                 } else if (point === '+') { //player on goal
-                    point = '.' //change to goal
+                    app.currentMap[row][column] = '.' //change to goal
                 }
             } else if (i === 1) {//player points can only change to player or player on goal
-                if (point === ' ') {//floor
-                    point = '@'//player
-                } else if (point === '.') {//goal
-                    point = '+'//player on goal
+                if (point === ' ' || point === '$') {//floor or box
+                    app.currentMap[row][column] = '@'//player
+                } else if (point === '.' || point === '*') {//goal or box on goal
+                    app.currentMap[row][column] = '+'//player on goal
                 }
             } else if (i === 2) {//following points only need to change to box or box on goal
-                if (app.currentMap[row][column] === '$') {
-
-                }
-                if (point === ' ') {//floor
-                    point = '$'//box
-                } else if (point === '.') {//goal
-                    point = '*'//box on goal
+                if (point === ' ' || point === '.') {
+                    if (prevPoint === '$') {
+                        app.currentMap[row][column] = '$'
+                    } else if (prevPoint === '*') {
+                        app.currentMap[row][column] = '*'
+                    }
                 }
             }
+        
         }
     }
 }
